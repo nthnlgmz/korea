767bd17a9bf4e5352b47037d6b01e12f7fd4076e
nthnlgmz <143614589+nthnlgmz@users.noreply.github.com>
2025-11-16 08:17:13 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // CHANGE THIS every deployment
-const CACHE_VERSION = "v5.2.3.2.8.6";
+const CACHE_VERSION = "v5.2.3.2.8.7";
 
 // Final cache name
 const CACHE_NAME = `kotrainer-${CACHE_VERSION}`;
